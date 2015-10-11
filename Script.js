e6160ea5ed887b0d079d56b98a5d9a94b70cc46e
Marx <mkont8@gmail.com>
2015-10-11 16:05:31 +0300

    failid <3

--- Script.js
+++ Script.js
@@ -5,25 +5,32 @@
 (function() {
 
     //start running tick every 1000ms
+    setInterval(tick, 1000);
+
 
 })();
 
 function getTime() {
-    //import time
-
+    //return current time
+    var x = new Date();
+    x.getHours();
+    x.getMinutes();
+    x.getSeconds();
 
 }
 
 function timeToBinClockTime(time) {
     //start writing binary numbers, fun fun fun!
+    //{"HoursTens" =>[0000]}
 }
 
 function setClockFace(binTimeRepr) {
     //which square is ON/OFF???
-
 }
 
 function tick() {
-    //
-
+    // update timer values
+    var CurrentTime=getTime();
+    var CurrentTimeBinary =timeToBinClockTime(CurrentTime);
+    setClockFace(CurrentTimeBinary);
 }
