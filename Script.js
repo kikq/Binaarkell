3e5ce2afd70c97a5846eabe10b193ae2e889e9db
Marx <mkont8@gmail.com>
2015-10-11 15:23:58 +0300

    failid <3

--- Script.js
+++ Script.js
@@ -1,3 +1,29 @@
 /**
  * Created by Margit on 11/10/2015.
  */
+
+(function() {
+
+    //start running tick every 1000ms
+
+})();
+
+function getTime() {
+    //import time
+
+
+}
+
+function timeToBinClockTime(time) {
+    //start writing binary numbers, fun fun fun!
+}
+
+function setClockFace(binTimeRepr) {
+    //which square is ON/OFF???
+
+}
+
+function tick() {
+    //
+
+}
